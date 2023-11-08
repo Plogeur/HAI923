b1decd1265b752ff0f2c02c56e3cedd2fd970a92
Plogeur <79613259+Plogeur@users.noreply.github.com>
2023-11-08 01:06:46 +0100

    Update pipeline.bash

--- pipeline.bash
+++ pipeline.bash
@@ -7,7 +7,7 @@
 
 # Prerequis object detection : 
     # pip3 install ultralytics
-    
+
 # Prerequis upscaler : 
     # git clone https://github.com/hollowaykeanho/Upscaler.git
     # brew install node
@@ -20,7 +20,9 @@ Output_file=""
 MinScale=64
 ScaleChange=false
 Upscale=4
+image_path=""
 
+# Initialize counter variables with default values
 DetectedObject=0
 FailDetection=0
 DeletedImage=0
@@ -28,16 +30,21 @@ DeletedImage=0
 # Define usage function to display usage instructions
 usage() {
     echo "Usage: $0 -i <input_file>"
-    echo "-i or -I <input_file>    Input file"
-    echo "-o or -O <output_file>   Output file, default/no argument : Crop_dataset_<input_file>"
-    echo "-s or -S <minimum_scale> Minimun scale for detected animal in the object detection step (minimum resolution = minimun scale * minimun scale). default/no argument : the most higher resolution picture in the detected animal picture"
-    echo "-u or -U <upscale_size>  Upscale size that will be applyed to the upscaling step, if u=1 there will be no upscaling (u∈[1;4]). default/no argument : u=4"
-    echo "-h or -H                 Display this help message"
+    echo "        -i or -I <input_file>    Input file"
+    echo "        -o or -O <output_file>   Output file, default/no argument : Crop_dataset_<input_file>"
+    echo "        -s or -S <minimum_scale> Minimun scale for detected animal in the object detection step 
+                                 (minimum resolution = minimun scale * minimun scale). 
+                                 default/no argument : the most higher resolution picture 
+                                 in the detected animal picture"
+    echo "        -u or -U <upscale_size>  Upscale size that will be applyed to the upscaling step, 
+                                 if u=1 there will be no upscaling 
+                                 (u∈[1;4]). default/no argument : u=4"
+    echo "        -h or -H                 Display this help message"
     exit 1
 }
 
 # Parse command-line arguments
-while getopts "iI:oO:sS:uU:hHcH" opt; do
+while getopts "i:I:o:O:s:S:u:U:hH" opt; do
   case $opt in
     i | I) # input dir
       Input_file="$OPTARG"
@@ -68,6 +75,12 @@ if [ -z "$Input_file" ]; then
   usage
 fi
 
+# Check if the file Upscaler/start.cmd is on the directory
+if [ ! -e "./Upscaler/start.cmd" ]; then
+  echo "File Upscaler/start.cmd does not exist in the directory."
+  exit 1
+fi
+
 # Check Minimum scale arguments
 if [ "$MinScale" -le 0 ] || [ "$MinScale" -gt 1081 ]; then
   echo "Error: MinScale must be between 1 and 1080"
@@ -80,23 +93,28 @@ if [ "$Upscale" -le 0 ] || [ "$Upscale" -gt 5 ]; then
   exit 1
 fi
 
-# Check if the directory exists
+# Check if the directory exists and no empty
 if [[ ! -d "$Input_file" ]]; then
-  echo "Error: directory not found"
+  echo "Error: Directory not found"
+  exit 1
+fi
+
+if [ -z "$(ls -A "$Input_file")" ]; then
+  echo "Error: Directory is empty"
   exit 1
 fi
 
 # Find the higher resolution animal pictures that is crop by yolo
 function higher_resolution() {
-    local source_directory="$Input_file"
+    local source_directory="Upscaled_dataset_$Input_file"
     local max_resolution=0
     local highest_resolution_image=""
     local list_images=""
 
     # Use find to locate all image files in the source directory
     while IFS= read -r image; do
-
         DetectedObject=$((DetectedObject + 1))
+        echo "Detected object : $DetectedObject"
         # Use identify from ImageMagick to get the resolution
         resolution=$(identify -format "%w:%h" "$image")
 
@@ -106,8 +124,9 @@ function higher_resolution() {
         # Calculate the product of height and width
         resolution_product=$((width * height))
 
-        if [ $resolution_product -gt $MinScale*$MinScale ]; then
+        if [[ $resolution_product -gt $((MinScale * MinScale)) ]]; then
           list_images+="$image"
+          echo "$resolution_product > $((MinScale * MinScale))"
           # Compare the resolution product with the current highest resolution
           if ((resolution_product > max_resolution)); then
               max_resolution=$resolution_product
@@ -117,11 +136,10 @@ function higher_resolution() {
     done < <(find "$source_directory" -type f -iname '*.jpg')
 
     if [ "$ScaleChange" = true ]; then
-      echo "$highest_resolution_image"
+      image_path="$highest_resolution_image"
       else 
-      echo "$list_images"
+      image_path="$list_images"
     fi 
-
 }
 
 function uspcaling() {
@@ -130,34 +148,38 @@ function uspcaling() {
 
     # Upscaled image size
     for picture in $Input_file/*; do
-        Upscaler/start.cmd --model upscayl-ultrasharp-v2 --scale $Upscale --format jpg --input $picture
-        picture_without_extension=$(basename "$picture" .jpg)
-        mv "$Input_file/$picture_without_extension-upscaled.jpg" "Upscaled_dataset_$Input_file/$picture_without_extension-upscaled.jpg"
-        rm -r "$Input_file/$picture_without_extension-upscaled_workspace"
+        if [ -f "$picture" ]; then
+            Upscaler/start.cmd --model upscayl-ultrasharp-v2 --scale $Upscale --format jpg --input $picture #-update -frames:v 1
+            picture_without_extension=$(basename "$picture" .jpg)
+            mv "$Input_file/$picture_without_extension-upscaled.jpg" "Upscaled_dataset_$Input_file/"
+            rm -r "$Input_file/$picture_without_extension-upscaled_workspace/"
+        fi
     done
 }
 
 function croping() {
-    # Create Crop_dataset directory
-    if [[ "$Output_file" == "" ]]; then
-        mkdir -p "Crop_dataset_$Input_file"
-        else
-        mkdir -p "$Output_file"
-    fi
-    
     # Crop animal object in picture
     for picture in "Upscaled_dataset_$Input_file"/*; do
         local SaveDetectionObject=$DetectedObject
         yolo predict model=yolov8x.pt source="$picture" save_crop
         source_directory="runs/detect/*/crops/*/*"
-        image_path=$(higher_resolution $source_directory)
+        higher_resolution $source_directory
         if [[ $SaveDetectionObject -eq $DetectedObject ]]; then 
             FailDetection=$((FailDetection + 1))
         fi
-        DeletedImage=$((DeletedImage + DetectedObject - (echo "$image_path" | wc -w))
-        for element in "$image_path"; do
-            mv "$element" Crop_dataset_$Input_file/
-        done
+        DeletedImage=$((DeletedImage + DetectedObject - DeletedImage - $(echo "$image_path" | wc -w)))
+        # Create Crop_dataset directory
+        if [[ "$Output_file" == "" ]]; then
+            mkdir -p "Crop_dataset_$Input_file"
+            for element in "$image_path"; do
+                mv "$element" Crop_dataset_$Input_file/
+            done
+        else
+            mkdir -p "$Output_file"
+            for element in "$image_path"; do
+                mv "$element" $Output_file/
+            done
+        fi
         rm -r runs/detect/
     done
 }
